6e0f75546d50097e8ac8264885c78ddc05d0f5e9
Gunasheelan Arullchelvan <140253087+Gunasheelan24@users.noreply.github.com>
2026-06-12 21:33:00 +0530

    Update Login.jsx

--- src/components/Home/Login.jsx
+++ src/components/Home/Login.jsx
@@ -6,7 +6,7 @@ import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
 import { useContext } from "react";
 import { Cookies, useCookies } from "react-cookie";
-import Store from "../../context/createStore";
+import Store from "../../context/createStore"; 
 import { useState } from "react";
 
 const Login = () => {
