6c4d034a55ca475b2217b5807caab2953ad63230
Gunasheelan Arullchelvan <140253087+Gunasheelan24@users.noreply.github.com>
2026-08-05 21:38:09 +0530

    Update Index.jsx

--- src/landingPage/Index.jsx
+++ src/landingPage/Index.jsx
@@ -1,10 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { disableReactDevTools } from "@fvilers/disable-react-devtools";
 import Home from "../components/reactRouter/Home";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle";
-
+import { disableReactDevTools } from "@fvilers/disable-react-devtools";
 if (import.meta.env.PROD === "production" || import.meta.env.PROD) {
   disableReactDevTools();
 }
